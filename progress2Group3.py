6c599f6937de39237d91924d55ba45476a1fedf9
flin82744 <flin82744@gmail.com>
2024-05-04 17:18:20 -0700

    add test functions block and print current time

--- progress2Group3.py
+++ progress2Group3.py
@@ -16,6 +16,10 @@ from datetime import datetime
 # By Fang Lin
 ################################################
 
+# Load Data Function:
+# Input: an csv file path
+# Output: a data frame with 18 specific column 
+
 def load_data(file_path):
 
     # Read the CSV file into a pandas DataFrame
@@ -38,18 +42,23 @@ def load_data(file_path):
                         'Humidity(%)', 'Pressure(in)', 'Visibility(mi)',
                         'Precipitation(in)', 'Weather_Condition']
 
-    # Keep only the specified columns in the DataFrame
-    df_filtered = df.loc[:, columns_to_keep]
+    try:
+        # Keep only the specified columns in the DataFrame
+        df_filtered = df.loc[:, columns_to_keep]
+    except KeyError:
+        return None
 
     return df_filtered
+
+# Process Data Function:
+# Input: a data frame
+# Output: a clean data frame
 
 def process_data(df):
 
     if df is None:
         print("No data to process. Please load data first.")
         return None, None
-
-    start_time = datetime.now()
 
     # Perform data cleaning tasks
     # 1 Eliminate rows with missing data in specified columns
@@ -76,7 +85,7 @@ def process_data(df):
     df_cleaned['End_Time'] = pd.to_datetime(df_cleaned['End_Time'])
     df_cleaned = df_cleaned[df_cleaned['End_Time'] - df_cleaned['Start_Time'] != pd.Timedelta(0)]
 
-    return df_cleaned, start_time
+    return df_cleaned
 
 #################################################
 # Questions From 1 to 5
@@ -412,12 +421,70 @@ def quit_program():
     quit()
 
 ###########################################################################
+#        Test Functions
+###########################################################################
+
+class Color:
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    RESET = '\33[0m'
+
+# Test all
+def test_all():
+    pass_test = False
+    if test_load_data():
+        pass_test = True
+        print(Color.GREEN + "\n------load_data() test1 passed test!\n" + Color.RESET)
+    else:
+        print(Color.RED+"------load_data() test1 not passed test!\n"+Color.RESET)
+
+    if test_process_data():
+        pass_test = True
+        print(Color.GREEN + "------process_data() test2 passed test!\n" + Color.RESET)
+    else:
+        print(Color.RED+"------process_data() test2 not passed test!\n"+Color.RESET)
+
+    return pass_test
+
+# Test load_data function: load an error file and get None 
+def test_load_data():
+    pass_load = False
+    test_df = load_data(test_file_path)
+
+    if (test_df) == None:
+        pass_load = True
+
+    return pass_load
+
+# Test process_data function: to compare the results
+# between reading file and load data
+def test_process_data():
+    pass_process = False
+    test_df =  pd.read_csv(file_path)
+    test_df_rows = len(test_df)
+    test_df_columns = len(test_df.columns)
+
+    loaded_df = load_data(file_path)
+    loaded_df_columns = len(loaded_df.columns)
+
+    if (test_df_columns != loaded_df_columns):
+        pass_process = True
+
+    processed_df = process_data(loaded_df)
+    processed_df_rows = len(processed_df)
+
+    if (test_df_rows != processed_df_rows):
+        pass_process = True
+
+    return pass_process
+
+###########################################################################
 #        Main Logic
 ###########################################################################
 
 # actual path to the CSV file
 file_path = 'US_Accidents_data.csv'
-#file_path = 'Boston_Lyft_Uber_Data.csv'
+test_file_path = 'Boston_Lyft_Uber_Data.csv'
 #file_path = 'InputDataSample.csv'
 
 # define Dataframe
@@ -426,7 +493,12 @@ df_filtered = pd.DataFrame(data)
 df_cleaned = pd.DataFrame(data)
 total_time = 0.0
 
-while True:
+# unit test
+pass_test = test_all()
+if not pass_test:
+    print("\nPlease check the functions that are not passed")
+
+while pass_test:
     print("(1) Load data\n"
       "(2) Process data\n"
       "(3) Print Answers\n"
@@ -444,6 +516,7 @@ while True:
 
         # Get the current time after loading the CSV
         current_time = datetime.now()
+        print(Color.GREEN)
         print("\nLoading and cleaning input data set:")
         print("*******************************************")
         print(f"[{current_time}] Starting Script")
@@ -460,6 +533,7 @@ while True:
         time_to_load = (current_time - start_time).total_seconds()
         total_time += (time_to_load/60)
         print(f"\nTime to load is: {time_to_load} seconds.\n")
+        print(Color.RESET)
 
     elif choice == '2':
 
@@ -467,41 +541,49 @@ while True:
             print ("\nPlease load data firstly!\n")
         else:
 
-            df_cleaned, current_time = process_data(df_filtered)
+            start_time = datetime.now()
+            df_cleaned = process_data(df_filtered)
 
+            current_time = datetime.now()
             # Print the cleaned DataFrame and its dimensions
+            print(Color.GREEN)
             print("\nProcessing input data set:")
             print("**********************************")
-            print(f"[{current_time}] Performing Data Clean Up")
+            print(f"[{start_time}] Performing Data Clean Up")
             total_rows = len(df_cleaned)
             print(f"[{current_time}] Total Rows Read after cleaning is:{total_rows}")
             # Calculate and print the time taken to clean the data
-            cleaning_time = (datetime.now() - current_time).total_seconds()
+            cleaning_time = (current_time - start_time).total_seconds()
             total_time += cleaning_time/60
             print(f"\nTime to process is: {cleaning_time} seconds.\n")
+            print(Color.RESET)
 
     elif choice == '3':
         if len(df_cleaned) == 0:
             print ("\nPlease process data firstly!\n")
         else:
+            print(Color.GREEN)
             print("\nAnswering questions:")
             print("**********************************")
 
-            start_time = datetime.now()
-            print(f"\n[{start_time}]What are the 3 months with the highest amount accidents reported?")
+            start_time1 = datetime.now()
+            print(f"\n[{start_time1}] What are the 3 months with the highest amount accidents reported?")
             df_q1 = q1_answer(df_cleaned)
             print(df_q1.head(3))
 
-            print("\nWhat is the year with the highest amount of accidents reported?")
+            start_time2 = datetime.now()
+            print(f"\n[{start_time2}] What is the year with the highest amount of accidents reported?")
             df_q2 = q2_answer(df_cleaned)
             print(df_q2.head(1))
 
-            print("\nWhat is the state that had the most accidents of severity 2? Display the data per year.")
+            start_time3 = datetime.now()
+            print(f"\n[{start_time3}] What is the state that had the most accidents of severity 2? Display the data per year.")
             df_q3_1, df_q3_2 = q3_answer(df_cleaned, 2)
             print(df_q3_1)
             print(f'\n{df_q3_2}')
 
-            print("\nWhat severity is the most common in Virginia, California and Florida?")
+            start_time4 = datetime.now()
+            print(f"\n[{start_time4}] What severity is the most common in Virginia, California and Florida?")
             df_q4_1 = q4_answer(df_cleaned, 'VA')
             df_q4_2 = q4_answer(df_cleaned, 'CA')
             df_q4_3 = q4_answer(df_cleaned, 'FL')
@@ -509,17 +591,20 @@ while True:
             print(f"In California: \n{df_q4_2}\n")
             print(f"In Florida: \n{df_q4_3}\n")
 
-            print("\nWhat are the 5 cities that had the most accidents in in California?Display the data per year.")
+            start_time5 = datetime.now()
+            print(f"\n[{start_time5}] What are the 5 cities that had the most accidents in in California?Display the data per year.")
             df_q5_CA, df_q5_max_five = q5_answer(df_cleaned)
             df_q5_1 = q5_answer_display_year(df_q5_CA, df_q5_max_five)
             print(f"In California: \n{df_q5_max_five}\n")
             print(f"The result is: \n{df_q5_1}\n")
 
-            print("What was the average humidity and average temperature of all accidents of severity 4 that occurred in Boston City? display the data per month.\n")
+            start_time6 = datetime.now()
+            print(f"\n[{start_time6}] What was the average humidity and average temperature of all accidents of severity 4 that occurred in Boston City? display the data per month.\n")
             df_q6 = q6_answer(df_cleaned, 4, 'Boston')
             print(f"The average of humidity and temerature: \n{df_q6}\n")
 
-            print("What are the 3 most common weather conditions (weather_conditions) when accidents occurred in New York? display the data per month.\n")
+            start_time7 = datetime.now()
+            print(f"\n[{start_time7}] What are the 3 most common weather conditions (weather_conditions) when accidents occurred in New York? display the data per month.\n")
             df_q7_NY, df_q7_most_three = q7_answer(df_cleaned, 'New York')
             df_q7_1, df_q7_2 = q7_answer_display_month(df_q7_NY, df_q7_most_three, 0, 0)
             print(f"The 3 most common in New York: \n{df_q7_most_three}\n")
@@ -529,52 +614,75 @@ while True:
             df_q7_5, df_q7_6 = q7_answer_display_month(df_q7_NY, df_q7_most_three, 2, 0)
             print(f"The third common is {df_q7_5}: \n{df_q7_6}\n")
 
-            print("What was the maximum visibility of all accidents of severity 2 that occurred in the state of New Hampshire?\n")
+            start_time8 = datetime.now()
+            print(f"\n[{start_time8}] What was the maximum visibility of all accidents of severity 2 that occurred in the state of New Hampshire?\n")
             df_q8 = q8_answer(df_cleaned, 'NH', 2)
             print(f"The maximum visibility in New Hsmpshire: \n{df_q8}\n")
 
-            print("How many accidents of each severity were recorded in Bakersfield? Display the data per year.\n")
+            start_time9 = datetime.now()
+            print(f"\n[{start_time9}] How many accidents of each severity were recorded in Bakersfield? Display the data per year.\n")
             df_q9 = q9_answer(df_cleaned, 'Bakersfield')
             print(f"The accidents' number of each severity level in Bakersfield: \n{df_q9}\n")
 
-            print("What was the longest accident (in hours) recorded in Las Vegas in the Spring (March, April, and May)? Display the data per year.\n")
+            start_time10 = datetime.now()
+            print(f"\n[{start_time10}] What was the longest accident (in hours) recorded in Las Vegas in the Spring (March, April, and May)? Display the data per year.\n")
             location_name = 'Las Vegas'
             spring_months = [3, 4, 5]
             df_q10 = q10_answer(df_cleaned, location_name, spring_months)
             print(f"The longest accident (in hours) recorded in Las Vegas in the Spring: \n{df_q10}\n")
-
-            query_time = (datetime.now() - start_time).total_seconds()
+            print(Color.RESET)
+            query_time = (datetime.now() - start_time1).total_seconds()
             total_time += query_time/60
 
     elif choice == '4':
         if len(df_cleaned) == 0:
             print ("\nPlease process data firstly!\n")
         else:
+            print(Color.GREEN)
             print("Search Accidents: ")
             print("********************************************")
+            start_time = datetime.now()
             res, df_filter_again = search_accidents_by_location(df_cleaned)
-            print(f"There were {res} accidents.\n")
+            print(f"\nThere were {res} accidents.\n")
+            search_time = (datetime.now() - start_time).total_seconds()
+            total_time += search_time/60
+            print(f"Time to perform search is: {search_time}\n")
+            print(Color.RESET)
 
     elif choice == '5':
         if len(df_cleaned) == 0:
             print ("\nPlease process data firstly!\n")
         else:
+            print(Color.GREEN)
             print("Search Accidents: ")
             print("********************************************")
+            start_time = datetime.now()
             res = search_accidents_by_date(df_cleaned)
-            print(f"There were {res} accidents.\n")
+            print(f"\nThere were {res} accidents.\n")
+            search_time = (datetime.now() - start_time).total_seconds()
+            total_time += search_time/60
+            print(f"Time to perform search is: {search_time}\n")
+            print(Color.RESET)
 
     elif choice == '6':
         if len(df_cleaned) == 0:
             print ("\nPlease process data firstly!\n")
         else:
+            print(Color.GREEN)
             print("Search Accidents: ")
             print("********************************************")
+            start_time = datetime.now()
             res = search_accidents_by_conditions(df_cleaned)
-            print(f"There were {res} accidents.\n")
+            print(f"\nThere were {res} accidents.\n")
+            search_time = (datetime.now() - start_time).total_seconds()
+            total_time += search_time/60
+            print(f"Time to perform search is: {search_time}\n")
+            print(Color.RESET)
 
     elif choice == '7':
+        print(Color.GREEN)
         print(f" Total Running Time (In Minutes): {total_time}")
+        print(Color.RESET)
         quit_program()
 
     else:
